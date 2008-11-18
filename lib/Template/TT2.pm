474469483e5ff923df2694042b4d9d1429b58b96
abw <abw@40303077-b156-0410-a00f-cb2aa0ea18bd>
2008-11-18 21:05:49 +0000

    Moved output and various other methods from Template::TT2 into Template::TT2::Hub
    git-svn-id: svn://svn.tt2.org/tt/Template-TT2/trunk@28 40303077-b156-0410-a00f-cb2aa0ea18bd

--- lib/Template/TT2.pm
+++ lib/Template/TT2.pm
@@ -18,168 +18,136 @@
 
 package Template::TT2;
 
-use Template::TT2::Modules;
 use Template::TT2::Class
     version    => 0.01,         # constructs VERSION and $VERSION
     debug      => 0,
-    base       => 'Template::TT2::Base',
+    base       => 'Template::TT2::Base Badger::Prototype',
     constants  => ':types',
     filesystem => 'FS VFS',
+    mutators   => 'hub',
     import     => 'class',
-    utils      => 'looks_like_number blessed',
-    constants  => 'TT2_MODULES',
+    utils      => 'looks_like_number blessed is_object',
+    constants  => 'TT2_HUB',
     constant   => {
         PRINT_METHOD => 'print',
-        SERVICE      => 'Template::TT2::Service',
     },
-    exports    => {
-        any    => 'TT2_MODULES',
+    config     => [
+        'HUB|hub|class:HUB|method:TT2_HUB',
+        'MKDIR|mkdir|class:MKDIR=1',
+        'ENCODING|encoding|class:ENCODING=0',
+        'OUTPUT|output|class:OUTPUT',
+        'OUTPUT_PATH|output_path|class:OUTPUT_PATH',
+        'QUIET|quiet=0',
+        'modules|MODULES|class:MODULES',
+    ],
+    messages    => {
+        hub_config => 'Configuration options are ignored when connecting to an existing hub',
     };
 
-our $VERSION       = 0.01;       # for ExtUtils::MakeMaker - not DRY, yuk
-our $MKDIR         = 1           unless defined $MKDIR;
-our $ENCODING      = 0           unless defined $ENCODING;
-our $OUTPUT        = \*STDOUT    unless defined $OUTPUT;
-our $OUTPUT_PATH   = undef       unless defined $OUTPUT_PATH;
-our $SERVICE       = SERVICE     unless defined $SERVICE;
-our $DEBUG_BINMODE = 0           unless defined $DEBUG_BINMODE;
-our @ARGS          = qw( MKDIR ENCODING OUTPUT OUTPUT_PATH );
+our $VERSION = 0.01;    # for ExtUtils::MakeMaker - not DRY, yuk
+our $OUTPUT  = \*STDOUT unless defined $OUTPUT;
 
-# alias modules() to return Template::TT2::Modules class constant
-*modules = \&TT2_MODULES;
 
 sub init {
     my ($self, $config) = @_;
     my $class = $self->class;
     my $debug = $config->{ DEBUG };
 
+    # call the imported (from Badger::Class::Config) configure() method
+    # to configure $self from $config according to the config spec above
+    $self->configure($config, $config);
+
+    # hub usually defaults to Template::TT2::Hub as defined by TT2_HUB 
+    # constant, but this can be overridden with a config option or pkg var
+    my $hub = $config->{ HUB }
+        || return $self->error_msg( missing => 'hub' );
+
+    if (ref $hub) {
+        if (is_object(TT2_HUB, $hub)) {
+            # we can attach to an existing hub object, but that means we 
+            # have to ignore any other config arguments
+            $self->warn_msg('hub_config')
+                unless $self->{ QUIET } || keys %$config == 1;
+    
+            $self->{ hub } = $hub;
+        }
+        else {
+            $self->error_msg( invalid => hub => $hub );
+        }
+    }
+    else {
+        # otherwise we've got a class name which we load and instantiate
+        $self->{ hub } = class($hub)->load->instance($config);
+    }
+
+    $self->debug("connected to hub: $self->{ hub }") if DEBUG;
+
     # convert textual DEBUG flags to number
 #    $config->{ DEBUG } = debug_flags($self, $debug)
 #        if defined $debug && ! looks_like_number $debug;
 
-    # set values or defaults for MKDIR, ENCODING, etc.
-    foreach my $arg (@ARGS) {
-        $self->{ $arg } 
-            = defined $config->{ $arg }
-                    ? $config->{ $arg }
-                    : $class->any_var($arg);
-    }
+    # TODO: delete this, make the hub create other objects and pass 
+    # hub reference along to context, filters, plugins, etc
 
     # create Badger::Filesystem::Directory object for file output,
     # check it exists, do a mkdir if the MKDIR flags says that's OK
-    if ($self->{ OUTPUT_PATH }) {
-        $self->debug("creating virtual filesystem for output in $self->{ OUTPUT_PATH }") if DEBUG;
-        my $dir = FS->directory(  $self->{ OUTPUT_PATH } )
-                    ->must_exist( $self->{ MKDIR       } );
-        $self->{ OUTPUT_FS } = VFS->new( root => $dir );
-    }
-    else {
-        $self->debug("output to filesystem") if DEBUG;
-        $self->{ OUTPUT_FS }  = FS;
-    }
-    
-    # load and instantiate service module
-    my $service = $config->{ SERVICE } || $SERVICE;
-    $self->{ SERVICE } = class($service)->load->instance($config);
-
-    # save config for lazy methods that might want it
-    $self->{ config } = $config;
+#    if ($self->{ OUTPUT_PATH }) {
+#        $self->debug("creating virtual filesystem for output in $self->{ OUTPUT_PATH }") if DEBUG;
+#        my $dir = FS->directory(  $self->{ OUTPUT_PATH } )
+#                    ->must_exist( $self->{ MKDIR       } );
+#        $self->{ OUTPUT_FS } = VFS->new( root => $dir );
+#    }
+#    else {
+#        $self->debug("output to filesystem") if DEBUG;
+#        $self->{ OUTPUT_FS }  = FS;
+#    }
+#    # save output filesystem back into config for context to share
+#    # with plugins, filters, etc., (e.g. redirect filter)
+#    $config->{ OUTPUT_FS } = $self->{ OUTPUT_FS };
+#    
 
     return $self;
 }
 
+#sub hub {
+#    return ref $_[0] eq HASH
+#        ? $_[0]->{ hub }
+#        : $_[0]->prototype->{ hub };
+#}
+
 sub process {
     my ($self, $input, $vars, @output) = @_;
     $vars ||= { };
-    $self->output( $self->{ SERVICE }->process($input, $vars), @output );
+    $self->output( $self->service->process($input, $vars), @output );
 }
-
-sub output {
-    my $self = shift;
-    my $text = shift;
-    my $dest = shift || $self->{ OUTPUT };
-    my $args = @_ == 1 && ref $_[0] eq HASH ? shift : { @_ };
-
-    # if no destination is specified and the OUTPUT config parameter
-    # is a false value then we return the text generated
-    return $text unless $dest;
-
-    # Otherwise we can have a plain text file name or a reference of
-    # some kind.  If we have an object with an overloaded stringification
-    # method (like a Badger::Filesystem::File), as accepted by textlike(),
-    # then we use that as a filename instead of treating it as an object.
-    my $type = ref $dest;
-#    $type = '' if textlike $dest;   # accept anything that has  file object
-
-    $self->debug("output [$type] => $dest\n") if DEBUG;
-
-    if (! $type) {
-        my $file = $self->output_file($dest);
-        $self->debug("output file: ", $file->definitive, "\n") if DEBUG;
-        my $fh   = $file->write;
-        my $enc  = defined $args->{ binmode  }
-                         ? $args->{ binmode  } 
-                 : defined $args->{ encoding }
-                         ? $args->{ encoding } 
-                 :         $self->{ ENCODING };
-
-        # TODO: move this into Badger::Filesystem:File
-        $fh->binmode($enc eq '1' ? () : $enc) if $enc;
-        $self->debug("DEBUG_BINMODE: $DEBUG_BINMODE") if DEBUG;
-        $DEBUG_BINMODE->($enc) if $DEBUG_BINMODE;   # hack for testing
-        $fh->print($text);
-        $fh->close;
-    }
-    elsif (blessed $dest) {
-        my $code = $dest->can(PRINT_METHOD)
-            || return $self->error_msg( bad_output => $dest );
-        return $code->($dest, $text);          # call object's print() method
-    }
-    elsif ($type eq CODE) {
-        return $dest->($text);          # call subroutine
-    }
-    elsif ($type eq GLOB) {
-        return print $dest $text;       # print to GLOB (e.g. STDOUT)
-    }
-    elsif ($type eq SCALAR) {
-        $$dest .= $text;                # append to text ref
-        return $dest;
-    }
-    elsif ($type eq ARRAY) {
-        push @$dest, $text;             # push onto list
-        return $dest;
-    }
-    else {
-        return $self->error_msg( bad_output => $dest );
-    }
-}
-
-sub output_file {
-    my $self = shift;
-    my $file = $self->{ OUTPUT_FS }->file(@_);
-
-    # make sure any intermediate directories between the OUTPUT_DIR and 
-    # final destination exist, or can be created if the MKDIR flag is set
-    $file->directory->must_exist($self->{ MKDIR });
-    
-    return $file;
-}
-
-#sub output_dir {
-#    shift->{ OUTPUT_DIR };
-#}
 
 sub service {
-    $_[0]->{ SERVICE };
+    return $_[0]->{ service }
+       ||= $_[0]->hub->service;
 }
 
 sub context {
-    $_[0]->{ SERVICE }->context;
+    return $_[0]->{ context }
+       ||= $_[0]->service->context;
+}
+
+sub output {
+    shift->hub->output(@_);
 }
 
 sub module {
+    shift->hub->module(@_);
+}
+
+sub destroy {
     my $self = shift;
-    TT2_MODULES->module(@_);
+    $self->{ hub }->destroy;
+    delete $self->{ hub };
+    return ();
+}
+
+sub DESTROY {
+    shift->destroy;
 }
 
 1;
