0f1dd19a96dc10c92805f55cacdda9eaac8ff082
abw <abw@40303077-b156-0410-a00f-cb2aa0ea18bd>
2008-12-02 18:35:05 +0000

    Added add_methods() to Template::TT2::Class
    git-svn-id: svn://svn.tt2.org/tt/Template-TT2/trunk@67 40303077-b156-0410-a00f-cb2aa0ea18bd

--- lib/Template/TT2/Class.pm
+++ lib/Template/TT2/Class.pm
@@ -4,12 +4,21 @@ use Badger::Debug ':debug';
 use Badger::Class
     version  => 0.01,
     uber     => 'Badger::Class',
+    utils    => 'self_params',
     constant => {
         base_id   => 'Template::TT2',
         CONSTANTS => 'Template::TT2::Constants',
         UTILS     => 'Template::TT2::Utils',
     };
 
-1;
 
-        
+sub add_methods {
+    my ($self, $params) = self_params(@_);
+    while (my ($name, $code) = each %$params) {
+        $self->method($name => $code)
+            unless $self->method($name);
+    }
+    return $self;
+}
+
+1;
